a53e8206c525552f09f695fc89af2a1db88f98d9
Lucas Juliano Possa Gomes <57335814+lucasjpg@users.noreply.github.com>
2021-11-23 15:37:11 -0300

    Add files via upload

---
+++ 1-Intro/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Para criar um projeto .NET:
+dotnet new console
+*/
+
+using System;
+
+namespace Parte_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundão");
+        }
+    }
+}
